4b1eaaccd4f50312416edfeca75d8cacdbdfd8f3
Kakarakis, Manos <emmanouil.kakarakis@accenture.com>
2021-11-15 20:40:18 +0200

    redirection routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [  
-  { path: 'home', component: HomeComponent }];
+const routes: Routes = [
+  // { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
