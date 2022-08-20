a2981c022cc54dda209e2eb051d9f97adb2806f8
Manos <m.kakarakis@doubleip.com>
2022-08-20 23:21:21 +0300

    env url

--- src/app/services/movieService.ts
+++ src/app/services/movieService.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class MovieService {
   status: string[] = ['OUTOFSTOCK', 'INSTOCK', 'LOWSTOCK'];
+  url = environment.url;
 
   constructor(private http: HttpClient) {}
 
@@ -15,18 +17,18 @@ export class MovieService {
   // }
 
   fetchMovies(): Observable<any> {
-    return this.http.get<any>(`http://localhost:3000/titles`);
+    return this.http.get<any>(`${this.url}/titles`);
   }
   fetchMovieId(req): Observable<any> {
-    return this.http.get<any>(`http://localhost:3000/titles/${req.id}`);
+    return this.http.get<any>(`${this.url}/titles/${req.id}`);
   }
   deleteMovieById(req): Observable<any> {
-    return this.http.delete<any>(`http://localhost:3000/titles/${req}`);
+    return this.http.delete<any>(`${this.url}/titles/${req}`);
   }
   updateMovieById(req, body): Observable<any> {
-    return this.http.put<any>(`http://localhost:3000/titles/${req}`, body);
+    return this.http.put<any>(`${this.url}/titles/${req}`, body);
   }
   createMovie(body): Observable<any> {
-    return this.http.post<any>(`http://localhost:3000/titles`, body);
+    return this.http.post<any>(`${this.url}/titles`, body);
   }
 }
