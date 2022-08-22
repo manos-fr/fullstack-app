231be12bdb2ae67653d076eb87bf9286ea4b0168
Manos <m.kakarakis@doubleip.com>
2022-08-22 13:03:31 +0300

    Revert "url of deployed server on heroku"
    This reverts commit 9ff91aa07d90a9766abea435b7a888d0fc7c2019.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'https://thawing-caverns-10070.herokuapp.com',
+  url: 'http://localhost:3000',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'https://thawing-caverns-10070.herokuapp.com',
+  url: 'http://localhost:3000',
 };
 
 /*
